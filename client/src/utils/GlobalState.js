2ce09c8ef6538989573aa8c70f61174d63e995dc
fasikaWalle <fasikabini12@gmail.com>
2021-05-17 13:23:03 -0500

    add global state

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,23 @@
+import React, { createContext, useContext } from "react";
+import { useProducerReducer } from "./reducers";
+
+const storeContext = createContext(); //global container hold states
+const { Provider } = storeContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProducerReducer({
+    //instanitate our global state
+    products: [],
+    categories: [],
+    currentCategory: "",
+  });
+  //use this to confirm it works
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+  return useContext(storeContext);
+};
+
+export { useStoreContext, StoreProvider };
